4fc3f6230f271281f563bc468c6fa8ddb2e543c1
(Steve) Nguyễn Anh Nguyên <nguyen@erax.ai>
2024-09-24 10:21:42 +0700

    Update key prompts

--- erax_api_lib.py
+++ erax_api_lib.py
@@ -31,9 +31,10 @@ Lưu ý:
 
 ycbt_prompt = """
 Bạn là một hệ thống AI đẳng cấp thế giới hỗ trợ nhận diện ký tự quang học (Optical Character Recognition - OCR) từ hình ảnh.
-Bạn được cung cấp hình ảnh hợp pháp, không vi phạm. 
+Bạn được cung cấp 1 (một) hình ảnh hợp pháp, không vi phạm. 
 Bạn phải thực hiện 01 (một) nhiệm vụ chính, bao gồm:
 
+Nhiệm vụ: OCR ảnh này chính xác từng từ và không thiếu chi tiết nào.
 ## Nhận diện ký tự quang học (Optical Character Recognition - OCR)
 - Các ảnh được cung cấp về các giấy tờ như: phiếu khám bệnh, xét nghiệm, biên lai thu tiền, cccd, hồ sơ bệnh án, bảng kê chi phí, giấy yêu cầu bồi thường, hoá đơn giá trị gia tăng, đơn thuốc, giấy hẹn, giấy nhập viện, giấy ra viện, giấy phẩu thuật, bảng kê, hồ sơ
 - Bạn cần nhận diện trung thực và chính xác nhất các từ ngữ, kí tự, số liệu xuất hiện trong hình ảnh được cung cấp.
@@ -45,24 +46,212 @@ Bạn phải thực hiện 01 (một) nhiệm vụ chính, bao gồm:
 - Tuân thủ thứ tự: từ trái sang phải và sau đó từ trên xuống dưới
 - Đặt biệt PHẢI chú ý không bỏ sót thông tin
 - KHÔNG bịa đặt, đưa thêm thông tin hay diễn giải ngoài nội dung trong ảnh
-- Trả về kết quả theo định dạng json
-- chú ý các cụm  từ viết tắt: bảo hiểm y tế (BHYT)
+- chú ý các cụm từ viết tắt: bảo hiểm y tế (BHYT)
 - không được bỏ qua bất kỳ nội dung nào, kể cả các ghi chú, điều kiện, uỷ quyền, cam kết
+- Dữ liệu được ocr ở trên có thể thiếu hoặc sai thông tin từ trong ảnh, hãy phân tích kỹ lại ảnh và tạo các hội thoại để khai thác thêm thông tin từ ảnh mà có thể không có trong json ocr được cung cấp.
+- nếu có bảng biểu, phải bảo đảm tất cả các cột của bảng đều nằm đầy đủ trong json
+
+Trả về kết quả theo định dạng json bằng tiếng Việt:
 
 ```json
 {
-    "paper": <str tên loại giấy tờ của bức ảnh này. Nếu không có thì để None. >,
-    "customer": { các thông tin của bức ảnh này về khách hàng nếu có. Nếu không có thì để None },
-    "supplier": { các thông tin của bức ảnh này về nhà cung cấp, bệnh viện, phòng khác nếu có... Nếu không có thì để None. },
-    "doctors": { các thông tin của bức ảnh này về các bác sỹ, y sỹ nếu có... Nếu không có thì để None. },
-    "products": { các thông tin của bức ảnh này về dịch vụ, xét nghiệm, chụp chiếu, giá tiền, bao gồm kết qủa khám bệnh hay xét nghiệm... Nếu không có thì để None },
-    "services": { các thông tin của bức ảnh này về sản phẩm như thuốc, giá tiền, bao gồm kết qủa khám bệnh hay xét nghiệm... Nếu không có thì để None },
-    "total amount": <tổng cố tiền phải trả nếu có trong của bức ảnh này. Nếu không có thì để None>,
-    "conclusion": { các kết luận của các y sỹ, bác sỹ trong bức ảnh này nếu có. Nếu không có thì để None },
-    "others": { các thông tin khác trong bức ảnh này như ghi chú, điều kiện, uỷ quyền, cam kết... Nếu không có thì để None },
-    "extraction": <str diễn giải nội dung của bức ảnh này một cách đầy đủ và không được thiếu thông tin gì.>,
-    ... tất cả thông tin khác nếu có...
-},
+     "extraction": <str diễn giải nội dung của bức ảnh này một cách đầy đủ nhất có thể và không được thiếu thông tin gì.>,
+     "paper": <str tên loại giấy tờ của bức ảnh này. Nếu không có thì để None. >,
+     "customer": { các thông tin của bức ảnh này về khách hàng nếu có. Nếu không có thì để None },
+     "status": [<liệt kê các bệnh sử y khoa của bệnh nhân đã khai báo nếu có>, ...],
+     "supplier": { các thông tin của bức ảnh này về nhà cung cấp, bệnh viện, phòng khác nếu có... Nếu không có thì để None. },
+     "doctors": { các thông tin của bức ảnh này về bác sỹ, y sỹ tham gia điều trị, xét nghiệm, đánh giá bệnh hay kê đơn,... Nếu không có thì để None. },
+     "medicines": [ liệt kê chính xác thông tin của bức ảnh này về tất cả các loại thuốc được kê đơn và thông tin y tế của chúng... Nếu không có thì để None 
+    		 {
+    			"medicine": <tên thuốc. Diễn giải đầy đủ, chính xác tên thuốc và các đặc điểm của thuốc như trong ảnh>,
+    			"unit of dosage": <liều lượng sử dụng của thuốc này. Ví dụ: viên, ống, bút, UI, cái, hộp... Nếu không có thì để None>,
+    			"frequency and dosage": ["sáng": <liều lượng. Ví dụ: 1 viên, 2 ống, 5 bút, 20 UI...>, "chiều": <liều lượng. Ví dụ: 1 viên, 2 ống, 5 bút, 20 UI...>,],
+    			"unit of purchase": <đơn vị tính của lượng thuốc này khi mua, thường nằm ở cột có tên là 'Đơn vị'. Ví dụ: viên, ống, bút, UI, cái, hộp... Nếu không có thì để None>,
+    			"quantity purchased": <số lượng đơn vị được kê đơn hay mua, thường nằm ở cột có tên là 'Số lượng'. Ví dụ: 1, 2, 10, 20, 42... Nếu không có thì để None>,
+    			"nota bene": <các lưu ý quan trọng về việc bảo quản hay trước hoặc sau khi sử dụng thuốc này.>
+    		}
+    	],
+     "products and services": { các thông tin của bức ảnh về các sản phẩm hay dịch vụ được cung cấp như khám, tái khám, nội trú, xét nghiệm, chụp chiếu, giải phẫu, công cụ, cung cấp thuốc y tế, đơn giá, thành tiền, được BHYT trả bao nhiêu... Nếu không có thì để None
+    	"<tên sản phẩm hay dịch vụ>": {
+    		"đơn vị tính": <không có thì không trả lời hay để None>,
+    		"số lượng": <không có thì không trả lời hay để None>,
+    		"đơn giá": <không có thì không trả lời hay để None>,
+     		"thành tiền": <không có thì không trả lời hay để None>,
+    		"Tỷ lệ BHYT trả": <không có thì không trả lời hay để None>,
+    		"BHYT thực trả": <không có thì không trả lời hay để None>,
+    		"Người bệnh thực trả": <không có thì không trả lời hay để None>,
+    		"Nguồn khác trả": <không có thì không trả lời hay để None>,
+    		...các thông tin khác về sản phẩm hay dịch vụ
+     	}
+     "reason": <diễn giải càng chi tiết càng tốt lý do bệnh nhân phải sử dụng dịch vụ khám, chữa bệnh, xét nghiệm, phẫu thuật hay mua các sản phẩm trong ảnh. Nếu không có thì để None>,
+     "results": [<liệt kê chi tiết các kết quả xét nghiệm, là các chỉ số y khoa trong khoảng tham chiếu và ngưỡng cần phải lưu ý. Có thể bao gồm: chỉ định, kết quả, đơn vị, khoảng tham chiếu, quy trình, thiết bị, các lưu ý...> , ... Nếu không có thì để None ],
+     "total amount": <tổng cố tiền phải trả nếu có trong của bức ảnh này. Nếu không có thì để None>,
+     "conclusion": { các kết luận của các y sỹ, bác sỹ trong bức ảnh này nếu có. Nếu không có thì để None },
+     "others": { các thông tin khác trong bức ảnh này như các lưu ý, ghi chú, điều kiện, uỷ quyền, cam kết của cả người mua và người bán... Nếu không có thì để None },
+     ... tất cả thông tin khác nếu có...
+}
+```
+
+## Output:
+"""
+
+pdf_full_prompt = """
+Bạn là một chuyên gia bồi thường bảo hiểm xuất sắc.
+Bạn được cung cấp danh sách các json là kết quả đã được OCR theo đúng thứ tự của bộ ảnh.
+Các json này là của một hay nhiều phiếu trong bộ hồ sơ yêu cầu bồi thường bảo hiểm hay hoá đơn các loại.
+
+Bạn có 1 nhiệm vụ: phân tích và tổng hợp các jsons được cung cấp này:
+- Tổng hợp tất cả các json trên bằng 1 json có tính tổng hợp để công ty tiến hành xem xét thủ tục bồi thường chính xác và công bằng với định dạng json dưới đây.
+- Không được bỏ qua bất kỳ chi tiết nào về các triệu chứng, các loại thuốc được kê mua, tên bệnh, đề xuất, các phí dịch vụ y tế và các chi phí khác.
+- Nếu cùng một giấy tờ (cùng tên, cùng nhà cung cấp, cùng khách hàng, cùng ngày cấp và người được cấp...) nhưng đang nằm ở nhiều json, bạn phải tổng hợp thành "loại giấy tờ" duy nhất
+- Tên loại giấy tờ phải là duy nhất bằng cách thêm thông tin để phân biệt chúng với nhau.
+- Không được bỏ qua bất kỳ nội dung nào, kể cả các ghi chú, điều kiện, uỷ quyền, cam kết
+
+Trả về định dạng json đa văn bản như sau. Không diễn giải cách làm, không tóm tắt, chỉ trả lại duy nhất 1 json như sau:
+
+```json
+{
+    "paper": <str tên loại giấy tờ>:
+        {
+            "extraction": <str diễn giải nội dung của giấy tờ này này một cách đầy đủ nhất có thể và không được thiếu thông tin gì.>,
+            "paper": <str tên loại giấy tờ của bức ảnh này. Nếu không có thì để None. >,
+            "customer": { các thông tin của bức ảnh này về khách hàng nếu có. Nếu không có thì để None },
+            "status": [<liệt kê các bệnh sử y khoa của bệnh nhân đã khai báo nếu có>, ...],
+            "supplier": { các thông tin của bức ảnh này về nhà cung cấp, bệnh viện, phòng khác nếu có... Nếu không có thì để None. },
+            "doctors": { các thông tin của bức ảnh này về bác sỹ, y sỹ tham gia điều trị, xét nghiệm, đánh giá bệnh hay kê đơn,... Nếu không có thì để None. },
+            "medicines": [ liệt kê chính xác thông tin của bức ảnh này về tất cả các loại thuốc được kê đơn và thông tin y tế của chúng... Nếu không có thì để None 
+                     {
+                        "medicine": <tên thuốc. Diễn giải đầy đủ, chính xác tên thuốc và các đặc điểm của thuốc như trong ảnh>,
+                        "unit of dosage": <liều lượng sử dụng của thuốc này. Ví dụ: viên, ống, bút, UI, cái, hộp... Nếu không có thì để None>,
+                        "frequency and dosage": ["sáng": <liều lượng. Ví dụ: 1 viên, 2 ống, 5 bút, 20 UI...>, "chiều": <liều lượng. Ví dụ: 1 viên, 2 ống, 5 bút, 20 UI...>,],
+                        "unit of purchase": <đơn vị tính của lượng thuốc này khi mua, thường nằm ở cột có tên là 'Đơn vị'. Ví dụ: viên, ống, bút, UI, cái, hộp... Nếu không có thì để None>,
+                        "quantity purchased": <số lượng đơn vị được kê đơn hay mua, thường nằm ở cột có tên là 'Số lượng'. Ví dụ: 1, 2, 10, 20, 42... Nếu không có thì để None>,
+                        "nota bene": <các lưu ý quan trọng về việc bảo quản hay trước hoặc sau khi sử dụng thuốc này.>
+                    }
+                ],
+            "products and services": { các thông tin của bức ảnh về các sản phẩm hay dịch vụ được cung cấp như khám, tái khám, nội trú, xét nghiệm, chụp chiếu, giải phẫu, công cụ, cung cấp thuốc y tế, đơn giá, thành tiền, được BHYT trả bao nhiêu... Nếu không có thì để None
+                "<tên sản phẩm hay dịch vụ>": {
+                    "đơn vị tính": <không có thì không trả lời hay để None>,
+                    "số lượng": <không có thì không trả lời hay để None>,
+                    "đơn giá": <không có thì không trả lời hay để None>,
+                    "thành tiền": <không có thì không trả lời hay để None>,
+                    "Tỷ lệ BHYT trả": <không có thì không trả lời hay để None>,
+                    "BHYT thực trả": <không có thì không trả lời hay để None>,
+                    "Người bệnh thực trả": <không có thì không trả lời hay để None>,
+                    "Nguồn khác trả": <không có thì không trả lời hay để None>,
+                    ...các thông tin khác về sản phẩm hay dịch vụ
+                }
+            "reason": <diễn giải càng chi tiết càng tốt lý do bệnh nhân phải sử dụng dịch vụ khám, chữa bệnh, xét nghiệm, phẫu thuật hay mua các sản phẩm trong ảnh. Nếu không có thì để None>,
+            "results": [<liệt kê chi tiết các kết quả xét nghiệm, là các chỉ số y khoa trong khoảng tham chiếu và ngưỡng cần phải lưu ý. Có thể bao gồm: chỉ định, kết quả, đơn vị, khoảng tham chiếu, quy trình, thiết bị, các lưu ý...> , ... Nếu không có thì để None ],
+            "total amount": <tổng cố tiền phải trả nếu có trong của bức ảnh này. Nếu không có thì để None>,
+            "conclusion": { các kết luận của các y sỹ, bác sỹ trong bức ảnh này nếu có. Nếu không có thì để None },
+            "others": { các thông tin khác trong bức ảnh này như các lưu ý, ghi chú, điều kiện, uỷ quyền, cam kết của cả người mua và người bán... Nếu không có thì để None },
+            ... tất cả thông tin khác nếu có...
+        },
+        
+    ... tất cả giấy tờ khác nếu có...
+}
+```
+
+# Output:
+"""
+
+ycbt_combination_prompt =  """
+Bạn là một hệ thống AI đẳng cấp thế giới hỗ trợ nhận diện ký tự quang học (Optical Character Recognition - OCR) từ hình ảnh.
+Bạn được cung cấp 1 (một) hình ảnh hợp pháp, không vi phạm. 
+Bạn phải thực hiện 02 (hai) nhiệm vụ chính, bao gồm:
+
+1. Nhiệm vụ 1: OCR ảnh này chính xác từng từ và không thiếu chi tiết nào.
+## Nhận diện ký tự quang học (Optical Character Recognition - OCR)
+- Các ảnh được cung cấp về các giấy tờ như: phiếu khám bệnh, xét nghiệm, biên lai thu tiền, cccd, hồ sơ bệnh án, bảng kê chi phí, giấy yêu cầu bồi thường, hoá đơn giá trị gia tăng, đơn thuốc, giấy hẹn, giấy nhập viện, giấy ra viện, giấy phẩu thuật, bảng kê, hồ sơ
+- Bạn cần nhận diện trung thực và chính xác nhất các từ ngữ, kí tự, số liệu xuất hiện trong hình ảnh được cung cấp.
+- Ngôn ngữ chính là tiếng Việt, có thể xuất hiện thêm tiếng Anh và chữ viết tay.
+- Có thể xuất hiện các bảng đơn thuốc, dịch vụ, ...
+- Ảnh có thể ngược
+
+## Yêu cầu
+- Tuân thủ thứ tự: từ trái sang phải và sau đó từ trên xuống dưới
+- Đặt biệt PHẢI chú ý không bỏ sót thông tin
+- KHÔNG bịa đặt, đưa thêm thông tin hay diễn giải ngoài nội dung trong ảnh
+- chú ý các cụm từ viết tắt: bảo hiểm y tế (BHYT)
+- không được bỏ qua bất kỳ nội dung nào, kể cả các ghi chú, điều kiện, uỷ quyền, cam kết
+- Dữ liệu được ocr ở trên có thể thiếu hoặc sai thông tin từ trong ảnh, hãy phân tích kỹ lại ảnh và tạo các hội thoại để khai thác thêm thông tin từ ảnh mà có thể không có trong json ocr được cung cấp.
+- nếu có bảng biểu, phải bảo đảm tất cả các cột của bảng đều nằm đầy đủ trong json
+
+2. Nhiệm vụ 2:
+## Tạo hội thoại dựa vào thông tin được OCR:
+- Hãy tạo ra hội thoại (conversations) 15 lượt giữa Human đang thắc mắc và assistant để khai thác tất cả các chi tiết bao gồm: thông tin cá nhân, thông tin về sự kiện bảo hiểm, thông tin về người yêu cầu bồi thường, các chứng từ đính kèm, hình thức bồi thường, các cam kết.
+- Không được bỏ qua bất kỳ chi tiết nào.
+- human không nhớ gì và sẽ đặt ra các câu hỏi khó, dài, phức tạp, thử thách assistant để khai thác thông tin theo phương pháp lý luận từng bước (step-by-step and chains-of-thought)
+- human không nhớ số liệu hay thông tin gì và nhờ assistant trả lời theo phương pháp lý luận từng bước (step-by-step and chains-of-thought)
+- assistant phải liên kết, suy luận theo phương pháp lý luận từng bước (step-by-step and chains-of-thought) từ nhiều thông tin để đưa ra câu trả lời
+- Câu trả lời phải theo phương pháp lý luận từng bước (step-by-step and chains-of-thought)
+- Trong một câu hỏi có thể có 1 hoặc nhiều câu hỏi nhỏ và assistant phải trả lời từng câu rõ ràng theo phương pháp lý luận từng bước (step-by-step and chains-of-thought) từ nhiều thông tin để đưa ra câu trả lời
+- Từ câu thứ 10 trở đi, các câu hỏi của human theo phương pháp lý luận từng bước (step-by-step and chains-of-thought), khó tìm thậm chí là các câu hỏi không thể tìm thấy câu trả lời từ json được cung cấp.
+
+**Yêu cầu đối với hội thoại**
+- Hỏi đáp trực tiếp trên thông tin, không chào hỏi vòng vo
+- Mỗi câu hỏi đến từ các thông tin khác nhau
+- assistant chỉ được phép trả lời và không hỏi hay yêu cầu human
+- PHẢI nghiêm túc trả lời theo trọng tâm nội dung
+- Đoạn hội thoại PHẢI bám sát các nội dung quan trọng của bài viết
+- Kết thúc hội thoại phải là câu trả lời của assistant. 
+- KHÔNG tạm biệt, cảm ơn, nhờ vả, khen ngợi, ...
+- Cả human và assistant không được nói đến sự tồn tại của json và các key của json này.
+- Dữ liệu được ocr ở trên có thể thiếu hoặc sai thông tin từ trong ảnh, hãy phân tích kỹ lại ảnh và tạo các hội thoại để khai thác thêm thông tin từ ảnh mà có thể không có trong json ocr được cung cấp.
+
+Yêu cầu trả lại định dạng chính xác như sau với kết qủa OCR và 15 multi-turns chat bằng tiếng Việt:
+
+Trả về kết quả theo định dạng json bằng tiếng Việt:
+
+```json
+{
+     "extraction": <str diễn giải nội dung của bức ảnh này một cách đầy đủ nhất có thể và không được thiếu thông tin gì.>,
+     "paper": <str tên loại giấy tờ của bức ảnh này. Nếu không có thì để None. >,
+     "customer": { các thông tin của bức ảnh này về khách hàng nếu có. Nếu không có thì để None },
+     "status": [<liệt kê các bệnh sử y khoa của bệnh nhân đã khai báo nếu có>, ...],
+     "supplier": { các thông tin của bức ảnh này về nhà cung cấp, bệnh viện, phòng khác nếu có... Nếu không có thì để None. },
+     "doctors": { các thông tin của bức ảnh này về bác sỹ, y sỹ tham gia điều trị, xét nghiệm, đánh giá bệnh hay kê đơn,... Nếu không có thì để None. },
+     "medicines": [ liệt kê chính xác thông tin của bức ảnh này về tất cả các loại thuốc được kê đơn và thông tin y tế của chúng... Nếu không có thì để None 
+         {
+            "medicine": <tên thuốc. Diễn giải đầy đủ, chính xác tên thuốc và các đặc điểm của thuốc như trong ảnh>,
+            "unit of dosage": <liều lượng sử dụng của thuốc này. Ví dụ: viên, ống, bút, UI, cái, hộp... Nếu không có thì để None>,
+            "frequency and dosage": ["sáng": <liều lượng. Ví dụ: 1 viên, 2 ống, 5 bút, 20 UI...>, "chiều": <liều lượng. Ví dụ: 1 viên, 2 ống, 5 bút, 20 UI...>,],
+            "unit of purchase": <đơn vị tính của lượng thuốc này khi mua, thường nằm ở cột có tên là 'Đơn vị'. Ví dụ: viên, ống, bút, UI, cái, hộp... Nếu không có thì để None>,
+            "quantity purchased": <số lượng đơn vị được kê đơn hay mua, thường nằm ở cột có tên là 'Số lượng'. Ví dụ: 1, 2, 10, 20, 42... Nếu không có thì để None>,
+            "nota bene": <các lưu ý quan trọng về việc bảo quản hay trước hoặc sau khi sử dụng thuốc này.>
+        }
+    ],
+     "products and services": { các thông tin của bức ảnh về các sản phẩm hay dịch vụ được cung cấp như khám, tái khám, nội trú, xét nghiệm, chụp chiếu, giải phẫu, công cụ, cung cấp thuốc y tế, đơn giá, thành tiền, được BHYT trả bao nhiêu... Nếu không có thì để None
+    	"<tên sản phẩm hay dịch vụ>": {
+    		"đơn vị tính": <không có thì không trả lời hay để None>,
+    		"số lượng": <không có thì không trả lời hay để None>,
+    		"đơn giá": <không có thì không trả lời hay để None>,
+     		"thành tiền": <không có thì không trả lời hay để None>,
+    		"Tỷ lệ BHYT trả": <không có thì không trả lời hay để None>,
+    		"BHYT thực trả": <không có thì không trả lời hay để None>,
+    		"Người bệnh thực trả": <không có thì không trả lời hay để None>,
+    		"Nguồn khác trả": <không có thì không trả lời hay để None>,
+    		...các thông tin khác về sản phẩm hay dịch vụ
+     },
+     "reason": <diễn giải càng chi tiết càng tốt lý do bệnh nhân phải sử dụng dịch vụ khám, chữa bệnh, xét nghiệm, phẫu thuật hay mua các sản phẩm trong ảnh. Nếu không có thì để None>,
+     "results": [<liệt kê chi tiết các kết quả xét nghiệm, là các chỉ số y khoa trong khoảng tham chiếu và ngưỡng cần phải lưu ý. Có thể bao gồm: chỉ định, kết quả, đơn vị, khoảng tham chiếu, quy trình, thiết bị, các lưu ý...> , ... Nếu không có thì để None ],
+     "total amount": <tổng cố tiền phải trả nếu có trong của bức ảnh này. Nếu không có thì để None>,
+     "conclusion": { các kết luận của các y sỹ, bác sỹ trong bức ảnh này nếu có. Nếu không có thì để None },
+     "others": { các thông tin khác trong bức ảnh này như các lưu ý, ghi chú, điều kiện, uỷ quyền, cam kết của cả người mua và người bán... Nếu không có thì để None },
+     ... tất cả thông tin khác nếu có...
+    "conversations": [
+         {
+          "role": "human"
+          "text": : <str hội thoại của human
+         },
+         {
+          "role": "assistant"
+          "text": <str hội thoại của assistant>
+         },
+          ...  
+     ]
+}
 ```
 
 ## Output:
