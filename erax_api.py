a3f917dd5006216ac78412e8fb0190583b701ad2
(Steve) Nguyễn Anh Nguyên <nguyen@erax.ai>
2024-09-24 09:59:35 +0700

    Simplying codes

--- erax_api.py
+++ erax_api.py
@@ -20,7 +20,7 @@ files = args.paths.split(",")
 Check erax_api_lib.py to:
 - use proper PROMPT which is VERY important
 - tune parameters likes temperature, top_p, top_k
-- only use 1-3 images for testing
+- only use 1-5 images for testing
 '''
 
 print ("""* Attention: please check erax_api_lib.py to: 
@@ -39,7 +39,11 @@ if files[0].split(".")[-1].lower()=="pdf":
         prompt =  PDF_prompt
         
     # PDF file
-    result, history =  API_PDF_Paths_OCR_EraX_VL_7B_vLLM(pdf_paths=files, prompt=PDF_prompt, erax_url=erax_url, API_key=API_key)
+    result, history =  API_PDF_OCR_EraX_VL_7B_vLLM(pdf_paths=files, 
+                                                   is_base64=False,
+                                                   prompt=PDF_prompt, 
+                                                   erax_url_id=erax_url_a100_id, 
+                                                   API_key=API_key_3P)
     
 else:
     if  args.prompt:
@@ -47,7 +51,11 @@ else:
     else:
         prompt =  ycbt_prompt
     # Images files
-    result, history =  API_Image_Paths_OCR_EraX_VL_7B_vLLM(image_paths=files, prompt=ycbt_prompt, erax_url=erax_url, API_key=API_key)
+    result, history =  API_Image_OCR_EraX_VL_7B_vLLM(image_paths=files, 
+                                                     is_base64=False,
+                                                     prompt=ycbt_prompt, 
+                                                     erax_url_id=erax_url_id, 
+                                                     API_key=API_key_3P)
 
 final_result =  {
     "text": result, 
